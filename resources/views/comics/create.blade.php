afcf6e6c4d2384dfc9895341d0610946321f4820
Amanjit313 <amanjitsingh313@gmail.com>
2022-07-04 15:58:41 +0200

    aggiunto al create l'errore in caso i dati non rispettino i parametri

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 
+    <div class="row">
+
+        @if($errors->any())
+        <div class="alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+    </div>
+
     <h2>Aperta pagina per creare un comic</h2>
 
     <form action=" {{ route('comics.store') }} " method="POST">
@@ -11,21 +25,33 @@
             <label for="title" class="form-label">Titolo</label>
             <input type="text" id="title"
                 name="title"
-                class="form-control" placeholder="Titolo">
+                value= "{{ old('title') }}"
+                class="form-control @error('title') is-invalid @enderror" placeholder="Titolo">
+                @error('title')
+                    <p class="error-msg text-danger">{{ $message }}</p>
+                @enderror
         </div>
 
         <div class="col-4">
             <label for="image" class="form-label">Image</label>
             <input type="text" id="image"
                 name="image"
-                class="form-control" placeholder="Image">
+                value="{{ old('image') }}"
+                class="form-control @error('image') is-invalid @enderror" placeholder="Image">
+                @error('image')
+                    <p class="error-msg text-danger">{{ $message }}</p>
+                @enderror
         </div>
 
         <div class="col-4">
             <label for="type" class="form-label">Type</label>
             <input type="text" id="type"
                 name="type"
-                class="form-control" placeholder="Type">
+                value="{{ old('type') }}"
+                class="form-control @error('type') is-invalid @enderror" placeholder="Type">
+                @error('type')
+                    <p class="error-msg text-danger">{{ $message }}</p>
+                @enderror
         </div>
 
         <button type="submit" class="btn btn-success">INVIA</button>
